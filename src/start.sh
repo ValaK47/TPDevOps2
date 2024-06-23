8edcd571d15957d7383e12f7410cf75e6fe969de
Valentin BOULANGER <95227681+ValaK47@users.noreply.github.com>
2024-06-23 19:27:00 +0100

    Update start.sh
    correction

--- src/start.sh
+++ src/start.sh
@@ -11,6 +11,12 @@ touch /tmp/started.time
 # This command creates a file named 'started.time' in the '/tmp' directory.
 # If the file already exists, the command does not change the file but updates its access and modification timestamps.
 
+if [ $? -ne 0 ]; then
+    exit
+fi
+# This conditional statement checks the exit status of the last command (touch command in this case).
+# If the exit status is not zero, which indicates an error, the script will exit immediately.
+
 date > /tmp/started.time
 # This command writes the current date and time to the 'started.time' file.
 # The '>' operator is used to redirect the output of the 'date' command to the file.
