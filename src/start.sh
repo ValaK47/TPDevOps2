a14b5ed36372157d4be101490fefd8239b12c670
Valentin BOULANGER <95227681+ValaK47@users.noreply.github.com>
2024-06-23 19:25:39 +0100

    Update start.sh
    changement des PATH, pour prendre un dossier qui n'est pas en READ ONLY

--- src/start.sh
+++ src/start.sh
@@ -1,23 +1,17 @@
 #!/bin/sh
 # This script is used to create a timestamp file and write the current date and time into it.
 
-set -eux
+set -eu
 # Setting shell options for better error handling and debugging.
 # 'e' option will cause the shell to exit if any invoked command fails.
 # 'u' option treats unset variables and parameters as an error.
-# 'x' option prints each command that gets executed to the terminal, useful for debugging.
 
-touch /home/www/started.time
-# This command creates a file named 'started.time' in the '/home/www' directory.
+# Création du fichier de timestamp dans un répertoire en écriture
+touch /tmp/started.time
+# This command creates a file named 'started.time' in the '/tmp' directory.
 # If the file already exists, the command does not change the file but updates its access and modification timestamps.
 
-if [ $? -ne 0 ]; then
-    exit
-fi
-# This conditional statement checks the exit status of the last command (touch command in this case).
-# If the exit status is not zero, which indicates an error, the script will exit immediately.
-
-date > /home/www/started.time
+date > /tmp/started.time
 # This command writes the current date and time to the 'started.time' file.
 # The '>' operator is used to redirect the output of the 'date' command to the file.
 
